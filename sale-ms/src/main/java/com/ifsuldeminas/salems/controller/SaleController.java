36cb313a6c720792b8756fcd6c9b8837541bf2c0
antonioamaral <antonio.amaral@sensedia.com>
2022-09-17 17:19:22 -0300

    Added sales-ms 100%

--- sale-ms/src/main/java/com/ifsuldeminas/salems/controller/SaleController.java
+++ sale-ms/src/main/java/com/ifsuldeminas/salems/controller/SaleController.java
@@ -1,4 +1,41 @@
 package com.ifsuldeminas.salems.controller;
 
+import com.ifsuldeminas.metricsms.dto.SaleSuccessDTO;
+import com.ifsuldeminas.metricsms.dto.SaleSumDTO;
+import com.ifsuldeminas.salems.dto.SaleDTO;
+import com.ifsuldeminas.salems.service.SaleService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/sales")
 public class SaleController {
+
+	@Autowired
+	private SaleService service;
+
+	@GetMapping
+	public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable) {
+		Page<SaleDTO> list = service.findAll(pageable);
+		return ResponseEntity.ok(list);
+	}
+
+	@GetMapping(value = "/amount-by-seller")
+	public ResponseEntity<List<SaleSumDTO>> amountGroupedBySeller() {
+		List<SaleSumDTO> list = service.amountGroupedBySeller();
+		return ResponseEntity.ok(list);
+	}
+
+	@GetMapping(value = "/success-by-seller")
+	public ResponseEntity<List<SaleSuccessDTO>> successGroupedBySeller() {
+		List<SaleSuccessDTO> list = service.successGroupedBySeller();
+		return ResponseEntity.ok(list);
+	}
+
 }
--- sale-ms/src/main/java/com/ifsuldeminas/salems/service/SaleService.java
+++ sale-ms/src/main/java/com/ifsuldeminas/salems/service/SaleService.java
@@ -1,4 +1,45 @@
 package com.ifsuldeminas.salems.service;
 
+import com.ifsuldeminas.metricsms.dto.SaleSuccessDTO;
+import com.ifsuldeminas.metricsms.dto.SaleSumDTO;
+import com.ifsuldeminas.salems.dto.SaleDTO;
+import com.ifsuldeminas.salems.entities.Sale;
+import com.ifsuldeminas.salems.repository.SaleRepository;
+import com.ifsuldeminas.sellerms.repository.SellerRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class SaleService {
+
+
+	@Autowired
+	private SaleRepository repository;
+
+	@Autowired
+	private SellerRepository sellerRepository;
+
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable) {
+
+		// TODO: 17/09/2022
+		sellerRepository.findAll();
+
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(SaleDTO::new);
+	}
+
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller() {
+		return repository.amountGroupedBySeller();
+	}
+
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return repository.successGroupedBySeller();
+	}
 }
